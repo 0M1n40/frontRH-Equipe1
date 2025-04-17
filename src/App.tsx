33d7add8aa2b314cff41097789351aed20ce2cd2
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-17 17:29:13 -0300

    fix(App): fix app routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import './App.css'
+import SobreNos from './pages/sobrenos/SobreNos'
+import Produtos from './pages/produtos/Produtos'
 
 
 
@@ -17,8 +19,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
-              <Route path="/sobre" element={<Home />} />
-              <Route path="/produtos" element={<Home />} />
+              <Route path="/sobre" element={<SobreNos />} />
+              <Route path="/produtos" element={<Produtos />} />
             </Routes>
           </div>
           
