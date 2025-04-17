3ac9e35649944d0d5bb0ad877a49cf90bd5c05ff
anakelys <souzanakely@gmail.com>
2025-04-17 16:11:10 -0300

    Botão

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,18 +6,25 @@ function Home() {
     return (
         <>
             <div className="home-background flex justify-center items-center min-h-screen px-0"> 
-                <div className="w-[90vw] flex justify-center">
+                <div className="w-[90vw] flex flex-col items-center gap-6">
+                    {/* Imagem */}
                     <img
                         src={ProjetoIntegradorSlide}
                         alt="Imagem Página Home"
                         className="w-full h-auto object-cover rounded-xl"
                     />
+
+                    {/* Botão abaixo da imagem */}
+                    <div className="botao-nova-postagem">
+                        Contate-nos
+                    </div>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
 export default Home;
+
 
 
